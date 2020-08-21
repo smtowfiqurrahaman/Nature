b887453422a924a39db28b8b6a483b6e5a4f35bf
S. M. Towfiqur Rahaman <62378943+smtowfiqurrahaman@users.noreply.github.com>
2020-08-21 19:19:57 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+//    OwlCarousel2 SECTION 
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+// wow section 
+  new WOW().init();
